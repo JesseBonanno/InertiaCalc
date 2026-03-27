63919b1b878d9676b9f57b6d024d40013a62b571
JesseBonanno <53243847+JesseBonanno@users.noreply.github.com>
2026-03-27 22:16:57 +1100

    add vite plugins array

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   // Blank config so that Cloudflare Wrangler has a file to modify during deploy
+  plugins: []
 });
